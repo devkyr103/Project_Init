80d58b0e5bc6b60f0fc9ab72a29b7b4617a5ae68
devkyr103 <devkyr103@gmail.com>
2023-02-23 17:22:42 +0900

    SAFE_DELETE 매크로

--- Client/Include/GameInfo.h
+++ Client/Include/GameInfo.h
@@ -8,8 +8,8 @@
 #include <crtdbg.h>
 #include <gdiplus.h>
 
-
-
-
 // GDI+
 #pragma comment(lib, "gdiplus.lib")
+
+#define SAFE_DELETE(p)	if(p)	{ delete p;	p = nullptr; }
+#define SAFE_DELETE_ARRAY(p)	if(p)	{ delete[] p;	p = nullptr; }
