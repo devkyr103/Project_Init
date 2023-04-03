0747029c78cc7c5f0470f27e09a8c9eff1a4a14e
devkyr103 <devkyr103@gmail.com>
2023-04-03 13:29:08 +0900

    키 코드 추가

--- Client/Include/CInput.cpp
+++ Client/Include/CInput.cpp
@@ -7,6 +7,10 @@ namespace kyr
 		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 		'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
 		'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+
+		VK_UP, VK_DOWN, VK_LEFT, VK_RIGHT, VK_SPACE,
+
+		VK_LBUTTON, VK_RBUTTON,
 	};
 
 	std::vector<CInput::Key> CInput::mKeys;
--- Client/Include/CInput.h
+++ Client/Include/CInput.h
@@ -8,6 +8,11 @@ namespace kyr
 		Q, W, E, R, T, Y, U, I, O, P,
 		A, S, D, F, G, H, J, K, L,
 		Z, X, C, V, B, N, M,
+
+		UP, DOWN, LEFT, RIGHT, SPACE,
+		
+		LBUTTON, RBUTTON,
+
 		END,
 	};
 
